9a38ff158ca23bce9ba3dcd84b99d611d7f6e598
neil <neil@local.com>
2016-02-25 19:51:08 +0800

    getpagenum:get url error

--- getEcommence.py
+++ getEcommence.py
@@ -100,12 +100,13 @@ def getPidList4Cat():
             catDb.update({'catId':cat['catId']},{'$push':{'timeline':{'lasttime':lastFreshDate,'count':catCount}}})
 
 def getCatPageNum(url):
-    r = session.get(url)
-    soup = BeautifulSoup(r.text)
-    strPages = soup.find('span',attrs={'class':'fp-text'})
-    if strPages:
+    pages = 0
+    try:
+        r = session.get(url)
+        soup = BeautifulSoup(r.text)
+        strPages = soup.find('span',attrs={'class':'fp-text'})
         pages = int(strPages.text.split('/')[1])
-    else:
+    except Exception, e:
         pages = 0
     return pages
 
